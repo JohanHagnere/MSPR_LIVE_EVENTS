6dbebf17072403186adb415510dbc9a349664ad7
florianC47 <florian.catusse@epsi.fr>
2023-05-15 16:13:15 +0200

    faq connecté

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\FaqRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Faq;
+
 
 class FaqController extends AbstractController
 {
     #[Route('/faq', name: 'app_faq')]
-    public function index(): Response
-    {
+    public function index(FaqRepository $faqRepository): Response
+    { $faqs= $faqRepository->findAll();
         return $this->render('faq/index.html.twig', [
-            'controller_name' => 'FaqController',
+            'faqs' => $faqs,
         ]);
     }
 }
+
+
