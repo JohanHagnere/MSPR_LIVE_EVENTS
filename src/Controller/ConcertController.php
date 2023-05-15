c187269c3e15f4bc2f0c1857fc58ee7fda83f079
Paul Castaings <castaings.paul@gmail.com>
2023-05-15 13:47:45 +0200

    controller concert fixed

--- src/Controller/ConcertController.php
+++ src/Controller/ConcertController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CConcertController extends AbstractController
+class ConcertController extends AbstractController
 {
     #[Route('/concert', name: 'app_concert')]
     public function index(): Response
