fcbae619f2257c275c21a28471dba35f1b649932
Pako65 <nico.carbo@orange.fr>
2023-05-16 13:28:43 +0200

    message red/yellow on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\MessageRepository;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(MessageRepository $messageRepository): Response
     {
+        $messages = $messageRepository->findBy(['type' => ['urgent', 'important']]);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'messages' => $messages,
         ]);
     }
 }
