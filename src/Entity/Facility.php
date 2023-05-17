6fb98ee062a70eb10da8c00edaa3842772d61e07
Johan Hagnere <johan.hagnere@gmail.com>
2023-05-17 15:26:48 +0200

    ajout description on facilities

--- src/Entity/Facility.php
+++ src/Entity/Facility.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\FacilityRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: FacilityRepository::class)]
@@ -30,6 +31,9 @@ class Facility
 
     #[ORM\Column(length: 255)]
     private ?string $img = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     public function getId(): ?int
     {
@@ -104,6 +108,18 @@ class Facility
     public function setImg(string $img): self
     {
         $this->img = $img;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
