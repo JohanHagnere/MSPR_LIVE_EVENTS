5b0a254c2411e8563b5ea9d08d4884b6283dd0db
florianC47 <florian.catusse@epsi.fr>
2023-05-17 15:18:25 +0200

    faq crud+admin

--- src/Form/FaqType.php
+++ src/Form/FaqType.php
@@ -6,14 +6,20 @@ use App\Entity\Faq;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 class FaqType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('question')
-            ->add('answer')
+            ->add('question',TextType::class, [ 'attr' => [
+                'class' => 'form-control-faq'
+                ]
+                ])  
+            ->add('answer',TextType::class, [ 'attr' => [
+                'class' => 'form-control-faq2'
+                ]
+                ])  
             //->add('festival')
         ;
     }
