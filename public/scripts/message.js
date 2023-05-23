87fb027d3fa49943b40182211537c62b9cd6f707
Pako65 <nico.carbo@orange.fr>
2023-05-23 16:45:08 +0200

    menu map et le reste

--- public/scripts/message.js
+++ public/scripts/message.js
@@ -1,13 +1,30 @@
 console.log("coucou");
 var toggleBtn = document.getElementById('toggleBtn');
-        var messageParent = document.getElementById('messageParent');
-        var currentIndex = 0;
+var messageParent = document.getElementById('messageParent');
+var currentIndex = 0;
 
-        function toggleMessage() {
-            currentIndex++;
-            if (currentIndex >= messages.length) {
-                currentIndex = 0;
-            }
-            messageParent.innerHTML = `<h1 id="messageContent" class=${messages[currentIndex].type}>${messages[currentIndex].content}</h1>`
-        }
-        toggleBtn.addEventListener('click', toggleMessage);
+function toggleMessage() {
+    currentIndex++;
+    if (currentIndex >= messages.length) {
+        currentIndex = 0;
+    }
+    messageParent.innerHTML = `<h1 id="messageContent" class=${messages[currentIndex].type}>${messages[currentIndex].content}</h1>`
+}
+toggleBtn.addEventListener('click', toggleMessage);
+
+let subMenu = document.querySelector('.sub-menu');
+let menuPlus = document.querySelector('.menu-plus');
+
+menuPlus.addEventListener('click', () => {
+    subMenu.classList.toggle('show')
+})
+
+
+
+// $(document).ready(function() {
+//     $('.footer-menu-item').click(function(e) {
+//         e.preventDefault();
+//         $(this).find('.sub-menu').toggle();
+//     });
+// });
+
