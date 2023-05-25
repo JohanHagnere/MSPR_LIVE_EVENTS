14dc113ed31a0a52ba6e0e3b5f121c8c83588ff0
Tommy Dumora <tommydumora@gmail.com>
2023-05-25 16:28:55 +0200

    performers

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\ConcertRepository;
 use App\Repository\FestivalRepository;
+use App\Repository\PerformerRepository;
+use App\Repository\SceneRepository;
 use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,18 +14,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdministrationController extends AbstractController
 {
     #[Route('/administration/{festivalId}', name: 'app_administration')]
-    public function index(FestivalRepository $festivalRepository, ConcertRepository $concertRepository, $festivalId): Response
+    public function index(FestivalRepository $festivalRepository, ConcertRepository $concertRepository, $festivalId, SceneRepository $sceneRepository, PerformerRepository $performerRepository): Response
     {
 
         $festival = $festivalRepository->find($festivalId);
-
         $concerts = $concertRepository->findByFestival($festival);
-
-        dump($concerts);
+        $performers = $performerRepository->findAll();
+        dump($performers);
 
         return $this->render('administration/index.html.twig', [
             'festival' => $festival,
             'concerts' => $concerts,
+            'performers' => $performers,
+            'festivalId' => $festivalId,
         ]);
     }
 }
