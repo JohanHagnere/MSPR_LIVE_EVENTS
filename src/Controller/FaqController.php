787046411f99eb627f4cf12a84c8b478b3413b48
Johan Hagnere <johan.hagnere@gmail.com>
2023-05-26 10:39:00 +0200

    faq wip

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -6,17 +6,19 @@ use App\Entity\Faq;
 use App\Entity\Festival;
 use App\Form\FaqType;
 use App\Repository\FaqRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity as ConfigurationEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[Route('/faq')]
+#[Route('{festival}/faq')]
+#[ConfigurationEntity('festival', expr: 'repository.find(festival)')]
 class FaqController extends AbstractController
 {
-    #[Route('/{id}/', name: 'app_faq_index_by_festival', methods: ['GET'])]
-    public function index(Festival $festival, FaqRepository $faqRepository): Response
+    #[Route('/', name: 'app_faq_index_by_festival', methods: ['GET'])]
+    public function index(Festival $festival, FaqRepository $faqRepository, Faq $faq): Response
     {
         return $this->render('faq/index.html.twig', [
             'faqs' => $faqRepository->findByFestival($festival),
@@ -24,9 +26,9 @@ class FaqController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/new', name: 'app_faq_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_faq_new', methods: ['GET', 'POST'])]
     #[IsGranted("ROLE_ADMIN")]
-    public function new(Festival $festival, Request $request, FaqRepository $faqRepository): Response
+    public function new(Festival $festival, Request $request, FaqRepository $faqRepository, Faq $faq): Response
     {
         $faq = new Faq();
         $faq->setFestival($festival);
@@ -36,7 +38,7 @@ class FaqController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $faqRepository->save($faq, true);
 
-            return $this->redirectToRoute('app_faq_index_by_festival', ['id' => $faq->getFestival()->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_faq_index_by_festival', ['festival' => $faq->getFestival()->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('faq/new.html.twig', [
@@ -45,17 +47,9 @@ class FaqController extends AbstractController
         ]);
     }
 
-    // #[Route('/{id}', name: 'app_faq_show', methods: ['GET'])]
-    // public function show(Faq $faq): Response
-    // {
-    //     return $this->render('faq/show.html.twig', [
-    //         'faq' => $faq,
-    //     ]);
-    // }
-
     #[Route('/{id}/edit', name: 'app_faq_edit', methods: ['GET', 'POST'])]
     #[IsGranted("ROLE_ADMIN")]
-    public function edit(Request $request, Faq $faq, FaqRepository $faqRepository): Response
+    public function edit(Request $request, Faq $faq, FaqRepository $faqRepository, Festival $festival): Response
     {
         $form = $this->createForm(FaqType::class, $faq);
         $form->handleRequest($request);
@@ -63,7 +57,7 @@ class FaqController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $faqRepository->save($faq, true);
 
-            return $this->redirectToRoute('app_faq_index_by_festival', ['id' => $faq->getFestival()->getId()], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_faq_index_by_festival', ['festival' => $faq->getFestival()->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('faq/edit.html.twig', [
@@ -74,13 +68,13 @@ class FaqController extends AbstractController
 
     #[Route('/{id}', name: 'app_faq_delete', methods: ['POST'])]
     #[IsGranted("ROLE_ADMIN")]
-    public function delete(Request $request, Faq $faq, FaqRepository $faqRepository): Response
+    public function delete(Request $request, Faq $faq, FaqRepository $faqRepository, Festival $festival): Response
     {
         $festival = $faq->getFestival();
-        if ($this->isCsrfTokenValid('delete'.$faq->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $faq->getId(), $request->request->get('_token'))) {
             $faqRepository->remove($faq, true);
         }
 
-        return $this->redirectToRoute('app_faq_index_by_festival', ['id' => $festival->getId()], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_faq_index_by_festival', ['festival' => $festival->getId()], Response::HTTP_SEE_OTHER);
     }
 }
